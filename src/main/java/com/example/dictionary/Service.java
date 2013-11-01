6a524ce6f427e3982a89bf98fe516b5500c599bd
kubamarchwicki <kuba@marchwicki.pl>
2013-11-01 15:55:39 +0100

    Initial Spring training code example

---
+++ src/main/java/com/example/dictionary/Service.java
@@ -0,0 +1,7 @@
+package com.example.dictionary;
+
+public interface Service {
+	
+	String getMessage();
+
+}
---
+++ src/test/java/com/example/dictionary/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package com.example.dictionary;
+
+import com.example.dictionary.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
