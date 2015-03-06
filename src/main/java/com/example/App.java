3ec054b2e34727c222cfc6e96992e42e745b0a1e
kubamarchwicki <kuba@marchwicki.pl>
2015-03-06 15:39:58 +0100

    Profiles in XML configuration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,18 @@
 package com.example;
 
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.example.dictionary.Controller;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String... args) {
 
-		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
-		Controller c = ctx.getBean(Controller.class);
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
+        ctx.getEnvironment().setActiveProfiles("jpa");
+        ctx.setConfigLocation("META-INF/spring/app-context.xml");
+        ctx.refresh();
+
+        Controller c = ctx.getBean(Controller.class);
 		c.run();
 		
 		ctx.close();
