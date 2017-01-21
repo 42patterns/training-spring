16de91092e5dacaa54742bfce1eed9866a06ec71
kubamarchwicki <kuba@marchwicki.pl>
2017-01-21 21:08:06 +0100

    End2End Integration testing

--- exercises/webmvc-integration-tests/src/test/java/com/example/web/WordControllerApplicationTest.java
+++ exercises/webmvc-integration-tests/src/test/java/com/example/web/WordControllerApplicationTest.java
@@ -1,43 +1,35 @@
 package com.example.web;
 
-import com.example.TestAppRunner;
-import com.example.AppConfiguration;
-import com.example.DispatcherConfig;
-import org.junit.Ignore;
+import com.github.mjeanroy.junit.servers.rules.TomcatServerRule;
+import com.github.mjeanroy.junit.servers.tomcat.EmbeddedTomcat;
+import com.github.mjeanroy.junit.servers.tomcat.EmbeddedTomcatConfiguration;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-@Ignore
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {TestAppRunner.class})
-@WebIntegrationTest(randomPort = true)
-@EnableAutoConfiguration
 public class WordControllerApplicationTest {
 
-    @Value("${local.server.port}")
-    private int port;
+    @ClassRule
+    public static TomcatServerRule server = new TomcatServerRule(
+            new EmbeddedTomcat(EmbeddedTomcatConfiguration.builder()
+                    .withBaseDir("./target/tomcat-work")
+                    .withWebapp("./target/war/")
+                    .build()));
 
     @Test
-    public void test_application() throws IOException {
+    public void test_application() throws IOException, InterruptedException {
+        final String location = "http://localhost:" + server.getPort() + "/search/dom.html";
+
         WebDriver driver = new HtmlUnitDriver();
-        driver.get("http://localhost:" + this.port + "/search/dom.html");
+        driver.get(location);
         assertEquals("Home", driver.getTitle());
 
         List<WebElement> li = driver.findElements(By.tagName("li"));
