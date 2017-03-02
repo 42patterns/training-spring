9dea2b0c738b3d4005ad1ea3de4cddd355c8ed5c
kubamarchwicki <kuba@marchwicki.pl>
2017-03-02 14:17:12 +0100

    Deffered result

--- exercises/webmvc-secured/src/main/java/com/example/web/WordsController.java
+++ exercises/webmvc-secured/src/main/java/com/example/web/WordsController.java
@@ -6,15 +6,18 @@ import com.example.dictionary.translation.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class WordsController {
@@ -35,8 +38,14 @@ public class WordsController {
     @RequestMapping(value = "/search/{word}", method = RequestMethod.GET,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     @ResponseBody
-    public List<DictionaryWord> search(@PathVariable String word) {
-        return service.getTranslationsForWord(word);
+    public DeferredResult<List<DictionaryWord>> search(@PathVariable String word) {
+        DeferredResult<List<DictionaryWord>> result = new DeferredResult<>();
+
+        CompletableFuture.supplyAsync(() -> service.getTranslationsForWord(word))
+            .thenAccept(dictionaryWords -> result.setResult(dictionaryWords));
+
+        return result;
+//        return service.getTranslationsForWord(word);
     }
 
     @RequestMapping(value = "/search/{word}.html", method = RequestMethod.GET)
